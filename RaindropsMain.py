9c7cfc8d36fb631226ad3ccd1667ed487fc5e047
Matthew Mackreth <MMackreth@spartaglobal.com>
2019-08-06 13:49:43 +0100

    Added comments to the raindrops function

--- RaindropsMain.py
+++ RaindropsMain.py
@@ -1,14 +1,16 @@
-def convert_number(n):
-    result_string = ""
-    sounds = {
+def convert_number(n):  # Function takes number as parameter
+    result_string = ""  # Initialises string to empty
+
+    sounds = {  # Uses dictionary to contain factors and strings for flexibility in the program
         3: 'Pling',
         5: 'Plang',
         7: 'Plong',
-    }
-    for sound in sounds.keys():
-        if n % sound == 0:
-            result_string += sounds[sound]
-    if result_string == "":
-        result_string = str(n)
+    }  # Dictionary contains factors with the corresponding sound string
 
-    return result_string
+    for sound in sounds.keys():  # For each key in the sounds dictionary
+        if n % sound == 0:  # Do number mod current key's value
+            result_string += sounds[sound]  # If current key is a factor then add the sound to the result string
+
+    if result_string == "":  # If no keys are factors the convert the string to number
+        result_string = str(n)  # assigns number converted to string to the result string
+    return result_string  # return the result string
